651f1e9d0b10d62f8e6aa6a7ca7b0ec84e83263a
Benson Michael Owen <lordkz19@gmail.com>
2024-10-17 18:01:56 +0100

    New style and login

--- controllers/chatPresenter.js
+++ controllers/chatPresenter.js
@@ -1,5 +1,10 @@
 const ChatPresenterPage = async (req,res) =>{
-    res.render("chatPresenterInterface", {email:req.params.email})
+   
+    if(req.cookies.posterUser){
+    res.render("chatPresenterInterface", {email:req.params.email, _token:req.cookies.posterUser})
+    }else{
+        res.render("loginExternal")
+    }
 }
 
 module.exports = ChatPresenterPage
